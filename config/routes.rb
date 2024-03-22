480472f56fc51a05a3e17ebcefb36d0873a86f88
Abby Castillo (work) <abbycastillo@Abbys-Mac-mini.local>
2024-03-21 17:15:45 -0700

    add root route to the top of Rails.application.routes.draw block

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "articles", to: "articles#index"
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
