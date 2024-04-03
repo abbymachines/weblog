05941c32accb67d4bb5c7149790967981f6e9952
Abby Castillo (work) <abbycastillo@Abbys-Mac-mini.local>
2024-04-03 13:55:56 -0700

    refactor spec name to make more sense

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,9 +3,9 @@
 require "rails_helper"
 
 RSpec.describe 'Comment', type: :request do
-  context 'created' do
-    it "without body returns 400 http status" do
-      headers = { 'ACCEPT' => 'application/json' }
+  context 'created without body' do
+    it "returns 400 http status" do
+      # headers = { 'ACCEPT' => 'application/json' }
       post '/comments', :params => { }
       expect(response).to have_http_status(400)
     end
