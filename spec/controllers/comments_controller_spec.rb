b0ccf6aada70f75fcb4e1f98a74dce34b166363b
Abby Castillo (work) <abbycastillo@Abbys-Mac-mini.local>
2024-04-03 14:48:07 -0700

    attempt to write a 200 http response spec, but unsure if it's working

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,11 +3,30 @@
 require "rails_helper"
 
 RSpec.describe 'Comment', type: :request do
+  before { @article = Article.new(title: 'all about lorems', body: 'ipsums and such. even a dolor sit amet', status: 'public') }
+  before { @topic = Topic.create(topic_name: 'celebrations') }
+
   context 'created without body' do
-    it "returns 400 http status" do
-      # headers = { 'ACCEPT' => 'application/json' }
+    it 'returns 400 http status' do
       post '/comments', :params => { }
       expect(response).to have_http_status(400)
+    end
+  end
+
+  context 'created with params' do
+    it 'returns 200 http status' do
+      article = @article
+      article.id = 1
+      article.save
+
+      topic = @topic
+      topic.id = 1
+      topic.save
+
+      headers = { 'ACCEPT' => 'application/json '}
+      post '/comments', :params => { :comment => {"commenter": "Abigail", "body": "hooray", "status": "public", "topic_id": 1, "article_id": 1 }}
+      # binding.pry
+      expect(response).to have_http_status(200)
     end
   end
 end
