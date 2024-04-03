f108481603953d51de5352c80a45841cab06b4f4
Abby Castillo (work) <abbycastillo@Abbys-Mac-mini.local>
2024-04-02 23:16:04 -0700

    add failing spec 'Comment but actually example string matches example string'

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,29 +2,20 @@
 
 # also: practice mocking
 
-# require "rails helper"
-
-# RSpec.describe CommentsController, type: :controller do
-#   describe "GET index" do
-#     it "has a 200 status code" do
-#       get :index
-#       expect(response.status).to eq(200)
-#     end
-#   end
-# end
-
-# RSpec.describe 'Post' do
-#   context 'before publication' do
-#     it 'cannot have comments' do
-#       expect { Post.create.comments.create! }.to raise_error(ActiveRecord::Invalid)
-#     end
-#   end
-# end
-
 RSpec.describe 'Comment' do
-  context 'before publication' do
-    it 'cannot have a topic' do
-      expect { Comment.create.topic_id.create! }.to raise_error(ActiveRecord::Invalid)
+  before { @example_string = 'hello rspec' }
+
+  context 'made without data' do
+    it 'has nil topic' do
+      expect(incomplete_comment.topic_id).to equal(nil)
+    end
+  end
+
+  context 'but actually example string' do
+    it 'matches example string' do
+      string = @example_string
+
+      expect(string).to equal('hello world')
     end
   end
 end
