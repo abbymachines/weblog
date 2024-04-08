07b4965e2e711fdf123caf70e7222113534e9147
Abby Castillo (work) <abbycastillo@Abbys-Mac-mini.local>
2024-04-08 08:52:11 -0700

    add require 'support/factory_bot' to rails_helper.rb

--- spec/factories/example_factories.rb
+++ spec/factories/example_factories.rb
@@ -2,4 +2,18 @@ FactoryBot.define do
   factory :topic do
     topic_name { "conversation starter" }
   end
+
+  factory :article do
+    title { "Place holding" }
+    body { "Lorem ipsum indeed" }
+    status { "public" }
+  end
+
+  factory :comment do
+    commenter { "sophie" }
+    body { "tomato soup can" }
+    status { "public" }
+    topic_id { 1 }
+    article_id { 1 }
+  end
 end
